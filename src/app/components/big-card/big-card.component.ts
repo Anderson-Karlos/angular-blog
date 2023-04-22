0afa26087d568f23017fbc67dbf122250cc52441
Anderson Karlos <andersonk.dev@gmail.com>
2023-04-22 08:26:54 -0300

    components dynamics property

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent {
-
+  
+@Input()  
+photoCover:string ="https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/26079ab060401d8a76039e985d63a3e9.jpe"
+@Input() 
+cardTitle:string ="Dragon Ball continua lançando episódios!"
+@Input() 
+cardDescription:string =""
 }
